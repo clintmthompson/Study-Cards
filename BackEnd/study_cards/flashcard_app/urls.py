6e385118b7be2ce032027bc2755f685507583a43
Mega Farts <clint.m.thompson@outlook.com>
2021-07-01 15:16:25 -0500

    Added get, put and delete views to both objects and updated paths

--- BackEnd/study_cards/flashcard_app/urls.py
+++ BackEnd/study_cards/flashcard_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('card/', views.CardList.as_view()),
-    path('deck/', views.DeckList.as_view())
+    path('card/<int:pk>/', views.CardDetail.as_view()),
+    path('deck/', views.DeckList.as_view()),
+    path('deck/<int:pk>/', views.DeckDetail.as_view()),
 ]
--- BackEnd/study_cards/flashcard_app/views.py
+++ BackEnd/study_cards/flashcard_app/views.py
@@ -6,7 +6,7 @@ from .serializers import DeckSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from json import JSONEncoder
+from django.http import Http404
 
 
 class CardList(APIView):
@@ -24,6 +24,33 @@ class CardList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class CardDetail(APIView):
+
+    def get_card(self, pk):
+        try:
+            return Card.objects.get(pk=pk)
+        except Card.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        card = self.get_card(pk)
+        serializer = CardSerializer(card)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        card = self.get_card(pk)
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        card = self.get_card(pk)
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class DeckList(APIView):
 
     def get(self, request):
@@ -37,4 +64,30 @@ class DeckList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeckDetail(APIView):
+    def get_deck(self, pk):
+        try:
+            return Deck.objects.get(pk=pk)
+        except Deck.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        deck = self.get_deck(pk)
+        serializer = DeckSerializer(deck)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        deck = self.get_deck(pk)
+        serializer = DeckSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        deck = self.get_deck(pk)
+        deck.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
