ebae689c8a51be59b82ad04e78de83fa6710d473
Mega Farts <clint.m.thompson@outlook.com>
2021-07-05 11:52:10 -0500

    Updated Collection component for layout

--- FrontEnd/study_cards/src/Components/Collection.jsx
+++ FrontEnd/study_cards/src/Components/Collection.jsx
@@ -9,13 +9,27 @@ const Collection = () => {
         
             <h1>Choose a collection</h1>
 
+
             <div className="card text-center">
                 <div className="card-body text-dark">
-                    <h4 className="card-title">Collection 1</h4>
-                    <p className="card-text-secondary">Description</p>
-                    <a href="#">View Collection</a>
+                    <h4 className="card-title">Collection 1</h4><br />
+                    <a href="#" className='btn btn-outline-primary'>View Collection</a>
                 </div>
             </div>
+            <div className="card text-center">
+                <div className="card-body text-dark">
+                    <h4 className="card-title">Collection 2</h4><br />
+                    <a href="#" className='btn btn-outline-primary'>View Collection</a>
+                </div>
+            </div>
+            <div className="card text-center">
+                <div className="card-body text-dark">
+                    <h4 className="card-title">Collection 3</h4><br />
+                    <a href="#" className='btn btn-outline-primary'>View Collection</a>
+                </div>
+            </div>
+
+
         </div>
 
         
