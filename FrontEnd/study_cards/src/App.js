87ca8ba9a77093ddbc9259671f6b0e50e5f3bb17
Mega Farts <clint.m.thompson@outlook.com>
2021-07-05 11:23:27 -0500

    Created Collection class with basic card template

--- FrontEnd/study_cards/src/App.js
+++ FrontEnd/study_cards/src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Components/Header';
+import Collection from './Components/Collection';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Collection />
     </div>
   );
 }
---
+++ FrontEnd/study_cards/src/Components/Collection.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+const Collection = () => {
+
+    return ( 
+        <div id='collection'>
+        
+            <h1>Choose a collection</h1>
+
+            <div className="card text-center">
+                <div className="card-body text-dark">
+                    <h4 className="card-title">Collection 1</h4>
+                    <p className="card-text-secondary">Description</p>
+                    <a href="#">View Collection</a>
+                </div>
+            </div>
+        </div>
+
+        
+     );
+}
+ 
+export default Collection;
