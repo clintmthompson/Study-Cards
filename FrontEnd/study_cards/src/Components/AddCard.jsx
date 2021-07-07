88ec33f74fc1d3df53e800cfeed7726a67f91525
Mega Farts <clint.m.thompson@outlook.com>
2021-07-07 13:06:25 -0500

    Changed position of form on the page

--- FrontEnd/study_cards/src/Components/AddCard.jsx
+++ FrontEnd/study_cards/src/Components/AddCard.jsx
@@ -7,14 +7,14 @@ class AddCard extends Component {
     }
     render() { 
         return ( 
-        <div style={{color: 'white'}}>
+        <div style={{color: 'white', textAlign: 'right', paddingRight: '200px'}}>
             <h1>Add Card to Collection</h1>
             <form onSubmit={(event) => this.handleSubmit(event)}>
                 <label htmlFor="word">New word: </label>
                     <input type="text" name="word"  onChange={this.handleChange} value={this.state.word}/><br />
                 <label htmlFor="definition">Definition: </label>
                     <input type="text" name="definition" onChange={this.handleChange} value={this.state.definition}/><br />
-                <button className='btn btn-info' type='submit'>Add to Collection</button>
+                <button className='btn btn-primary' type='submit'>Add to Collection</button>
             </form>
                
         </div>
